72ccacb3ccb2cfc661b349f0070dae515a610daa
Jeremiah Sullivan <sully42@gmail.com>
2019-10-22 14:36:06 -0400

    Initial commit after fork

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # EC601-A2-Acoustic-Comm
+Branch begun by Glenn Martinez
+Modified by Jeremiah Sullivan
+
+Based on example code that can be found here 
+# https://github.com/chirp/chirp-python-examples
+
+Requires importing tokens to a ~/.chirprc file
+# https://developers.chirp.io/applications
+
+---
+This branch may serve more as a prototyping area for signal processing validation
+
--- messenger.py
+++ messenger.py
@@ -1,12 +1,8 @@
-#Example code taken from github 
-#user joextodd https://github.com/chirp/chirp-python-examples
-
 import argparse
 import time
-
+from math import floor
 from chirpsdk import ChirpSDK, CallbackSet
 
-#PASTE API KEYS HERE
 
 class Callbacks(CallbackSet):
 
@@ -25,7 +21,7 @@ def main(args):
     # ------------------------------------------------------------------------
     # Initialise the Connect SDK.
     # ------------------------------------------------------------------------
-    sdk = ChirpSDK(app_key,app_secret,app_config)
+    sdk = ChirpSDK()
     print(sdk.audio.query_devices())
     print(str(sdk))
     sdk.audio.output_device = args.o
@@ -33,14 +29,17 @@ def main(args):
         sdk.set_config_from_network()
 
     if sdk.protocol_name != '16khz-mono':
+        print("The protocol name is: " + sdk.protocol_name)
         raise RuntimeError('Must use the 16khz-mono protocol ' +
                            'to be compatible with other Chirp Messenger apps.')
-
+    else: 
+        print(sdk.protocol_name)
     # ------------------------------------------------------------------------
     # Parse unicode and send as a chirp payload
     # ------------------------------------------------------------------------
-    message = args.message.encode('utf-8')
-    payload = sdk.new_payload(message)
+    message   = args.message.encode('utf-8')
+    waitTime  = args.wait
+    payload   = sdk.new_payload(message)
 
     sdk.volume = args.volume
     sdk.set_callbacks(Callbacks())
@@ -49,8 +48,16 @@ def main(args):
 
     try:
         # Process audio streams
-        while True:
-            time.sleep(0.1)
+        tic =  time.time()
+        elapsedFirst = 0; 
+        elapsed      = 0;
+        while elapsed < waitTime:
+                time.sleep(0.1)
+                elapsed = time.time() - tic
+                elapsedLast = floor(elapsed)
+                if elapsedLast > elapsedFirst:
+                    elapsedFirst = elapsedLast
+                    print(f"Waiting for messages ({elapsedLast} s of {waitTime} s)")
     except KeyboardInterrupt:
         print('Exiting')
 
@@ -65,7 +72,8 @@ if __name__ == '__main__':
         description='Chirp Messenger',
         epilog="Send a message to other Chirp Messengers"
     )
-    parser.add_argument('message', help='Text or emoji message to send')
+    parser.add_argument('--wait',  help='Time to wait for a Rx message (seconds)', default = 5.0, type = float)
+    parser.add_argument('-m', '--message', help='Text or emoji message to send', type = str)
     parser.add_argument('-v', '--volume', help='Volume', default=1.0)
     parser.add_argument('-o', type=int, default=None, help='Output device index (optional)')
     parser.add_argument('--network-config', action='store_true', help='Optionally download a config from the network')
