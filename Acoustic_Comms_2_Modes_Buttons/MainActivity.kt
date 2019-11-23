41719e513d522e00409f3cb758c69a4d00671708
janische <55109259+janische@users.noreply.github.com>
2019-11-23 13:54:41 -0500

    Add files via upload

--- Acoustic_Comms_2_Modes_Buttons/MainActivity.kt
+++ Acoustic_Comms_2_Modes_Buttons/MainActivity.kt
@@ -10,9 +10,10 @@ import android.widget.TextView
 import android.widget.Toast
 
 private const val REQUEST_RECORD_AUDIO = 1
-const val CHIRP_APP_KEY = "";
-const val CHIRP_APP_SECRET = "";
-const val CHIRP_APP_CONFIG = "";
+
+const val CHIRP_APP_KEY = "37b57Bf1bCEA0Ca8e9Ba3CfCc";
+const val CHIRP_APP_SECRET = "8fdD20Fa0E7A7b4A0AEC2E7b73bc5BCAF9e520bBAd8AA2FbCE";
+const val CHIRP_APP_CONFIG = "lqAQODNUj7tlmxHHz569+BqmDEuYfhe41a6GkRsE8z0NOuRU0eFKuOM5XWv6E5PaBO4ikqV45sS5O8+5wfb7UUhS9V2hKc3pBQcrlThxittYkeC7N5UX4VmEVwQY2T/VtZddGWrH9AQcDwoOzvAC0l3+Z4Bx71WAQJR2+aMEiUqVazuLV+o5hKU0EtlKEtDR/oOppewLNr2ZKs9zgli+92FVFB+inM3EkLjVk00u8dlZK6ZFbmVKK+qGxWOOn+T7cKPX8zmEjy1G/IC7BM84t6rjLnu3vgvFzb5ST+pkAb9Q7aMZBpMZI0c0hWnirOjqT/mGvr3xSkm4Qn2zsQFZvQvhGhx/I/gp+KQbE7XKTuiknrYbj58rOQxbcTQgDAs6epgd0MxSZlb9gzq/KBKymSWJK7qywqKbHr/cllaYNPN9w0hA+SAI9CuNe6GjZCXVU/GMsDFDeDXdSaG0p04/hEAj96c5CPYT1XMv+GXB0tbYyAtx+AzJXO5NC5ldJymG3H2C8WZjNjrczVETI0vivyNCQOh4z242jMKIeXrGcskX5b70uX81E+3VX7/6jwmy+97+4JiIR5yPRiX7vaLNFywyeEoppray0WRL/FhakyDP7Wkey5tDdX5izu2AzPISs8bYEAmvrFspGP3QPWVQoGgCqa+vyjXE9oXBBK8VH8toHPlIvFkmMPpAC4zhZs/ALiBUyuLHqXQez2p6du0jTtA+A7OqZtMKvyC3MRO7We1QAXebqYtPbwsWGIKOvkYiROmig+CI+cE0usPCJcttOGFnq209Gjx5q6X2GdASp9G772go4P7jBCLIkg5Jq36xZQWWQ5L1v3/TBKPW/NBquBaHHpOcjJCo6SA0h7Me7OI=";
 
 
 class MainActivity : AppCompatActivity() {
@@ -58,5 +59,20 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+    fun setContacts(): MutableList<String> {
+        var contacts = mutableListOf<String>()
+        contacts.addAll(listOf("JP", "Glenn", "Jonathan", "Amanda"))
+        return contacts
+    }
+    fun assignIDs(contacts: MutableList<String>): MutableMap<Int, String> {
+        var IDs = mutableMapOf<Int, String>()
+        IDs[0] = "Broadcast"
+        var i = 1
+        for (con in contacts) {
+            IDs[i] = con
+            i++
+        }
+        return IDs
     }
 }
